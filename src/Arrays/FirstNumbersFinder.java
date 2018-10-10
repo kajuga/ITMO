9aa4c41009baf83a5465225ea48d4a0f89c8e1d6
kajuga <msg2fedorov@gmail.com>
2018-10-10 14:29:56 +0300

    Дан массив и число K. Найдите первые K самых часто встречающихся элементов. доделать

---
+++ src/Arrays/FirstNumbersFinder.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class FirstNumbersFinder {
+
+    public static void main(String[] args) {
+        int K = 3;
+        int[] topNumber = new int[K];
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.print(array[i] + " ");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+
+
+        }
+    }
+
+    public boolean frequencyCounter (int[] array, int value) {
+        boolean result = false;
+        int[] temp = array;
+        int counter = 0;
+
+
+        return result;
+    }
+}
